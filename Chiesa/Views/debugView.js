fe6d22a6466fe6b614228fbcdcf88c0340bd84db
Emanuele Tinari <82208785+EmanueleTinari@users.noreply.github.com>
2026-01-20 09:10:01 +0100

    Delete debugView.js

--- Chiesa/Views/debugView.js
+++
@@ -1,7 +0,0 @@
-module.exports = (dv, input) => {
-    dv.paragraph("🔎 debug attivato");
-    const jsFiles = app.vault.getAllLoadedFiles()
-        .filter(f => f.path.endsWith(".js"))
-        .map(f => f.path);
-    dv.list(jsFiles);
-};
